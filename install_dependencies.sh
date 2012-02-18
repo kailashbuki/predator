6fadd8ded99ddb444face2379f69a0d957fb64b2
Kailash <kailash.buki@gmail.com>
2012-02-18 19:51:13 +0545

    wget used again

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,6 +2,11 @@
 
 sudo mkdir -p /tmp
 
+cd /tmp
+
+echo 'Installing curl first'
+sudo apt-get install curl
+
 echo 'Downloading & installing python development headers'
 sudo apt-get install python-dev
 
@@ -9,10 +14,10 @@ echo 'Downloading & installing distribute'
 sudo curl http://python-distribute.org/distribute_setup.py | sudo python
 
 echo 'Downloading libevent'
-curl -o /tmp/libevent-2.0.17-stable.tar.gz https://github.com/downloads/libevent/libevent/libevent-2.0.17-stable.tar.gz
+wget https://github.com/downloads/libevent/libevent/libevent-2.0.17-stable.tar.gz
 echo 'installing libevent'
 tar -C /tmp -xzf /tmp/libevent-2.0.17-stable.tar.gz
-cd /tmp/libevent-20.17-stable
+cd /tmp/libevent-20.17
 ./configure
 make
 sudo make install
@@ -27,7 +32,7 @@ echo 'Downloading nginx'
 sudo pip install nginx
 
 echo 'Downloading zeromq'
-curl -o /tmp/zeromq-2.1.11.tar.gz http://download.zeromq.org/zeromq-2.1.11.tar.gz
+wget http://download.zeromq.org/zeromq-2.1.11.tar.gz
 echo 'Installing zeromq'
 tar -C /tmp -xzf /tmp/zeromq-2.1.11.tar.gz
 cd /tmp/zeromq-2.1.11
@@ -39,7 +44,7 @@ echo 'Downloading pyzmq'
 sudo pip install pyzmq
 
 echo 'Downloading libpng'
-curl -o /tmp/libpng-1.5.7.tar.xz http://downloads.sourceforge.net/project/libpng/libpng15/older-releases/1.5.7/libpng-1.5.7.tar.xz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Flibpng%2Ffiles%2F&ts=1329403713&use_mirror=space
+wget http://downloads.sourceforge.net/project/libpng/libpng15/older-releases/1.5.7/libpng-1.5.7.tar.xz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Flibpng%2Ffiles%2F&ts=1329403713&use_mirror=space
 echo 'Installing libpng'
 tar -C /tmp -xzf /tmp/libpng-1.5.7.tar.xz
 cd /tmp/libpng-1.5.7
@@ -48,7 +53,7 @@ make
 sudo make install
 
 echo 'Downloading libpoppler'
-curl -o /tmp/poppler-0.18.4.tar.gz http://poppler.freedesktop.org/poppler-0.18.4.tar.gz
+wget http://poppler.freedesktop.org/poppler-0.18.4.tar.gz
 echo 'Installing libpoppler'
 tar -C /tmp -xzf /tmp/poppler-0.18.4.tar.gz
 cd /tmp/poppler-0.18.4
@@ -57,7 +62,7 @@ make
 sudo make install
 
 echo 'Downloading mongodb 64-bit'
-curl -o /tmp/mongodb-linux-x86_64-2.0.2.tgz http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.0.2.tgz
+wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.0.2.tgz
 echo 'Installing mongodb'
 tar -C /tmp -xzf /tmp/mongodb-linux-x86_64-2.0.2.tgz
 cd /tmp/mongodb-linux-x86_64-2.0.2
